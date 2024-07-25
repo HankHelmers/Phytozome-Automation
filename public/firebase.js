7cbb056170ea6d40aaf61eaec0984429f29b0157
Hank Helmers <89431423+HankHelmers@users.noreply.github.com>
2024-07-25 16:13:15 -0400

    Deployment v0.0.1
    Basic linking setup to share with lab. Will connect additional functionality in following week.

---
+++ public/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+
+console.log("Firebase connected.")
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  
+};
+
+const app = initializeApp(firebaseConfig);
